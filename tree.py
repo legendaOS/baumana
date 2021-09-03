71902bbbc845808f96e4958d432eb060ea1f844b
legendaOS <robolegenda@gmail.com>
2021-09-03 21:24:17 +0300

    багофикс разметка

---
+++ tree.py
@@ -0,0 +1,51 @@
+class MessangerInterface():
+    def Message():
+        pass
+
+class NodeInterface(MessangerInterface):
+    def Function():
+        pass
+
+    def __init__(self, Parent = None, Child = None) -> None:
+        self.Parent = Parent
+        self.Child = Child
+
+class Node(NodeInterface):
+    def __init__(self, Parent = None, Child = []) -> None:
+        super().__init__(Parent=Parent, Child=Child)
+
+    def Function(self):
+        pass
+        
+class Product(NodeInterface):
+    def __init__(self, Price = 0, Sale = 0, Available = True,
+    Categories = [], Feedback  = [], Adress  = [], 
+    Parent = None, Child = None) -> None:
+        super().__init__(Parent=Parent, Child=Child)
+        self.Price = Price
+        self.Sale = Sale
+        self.Available = Available
+        self.Categories = Categories
+        self.Feedback = Feedback
+        self.Adress = Adress
+
+    def Function(self):
+        pass
+
+class Sign():
+    def __init__(self, Name = '', Type = 'feedback', Value = 'отлично') -> None:
+        self.Name = Name
+        self.Type = Type
+        self.Value = Value
+    
+    def Compare(self):
+        if self.Type == 'feedback':
+            table = {'отлично':5, 'хорошо':4, 
+            'нормально':3, 'плохо':2, 'ужасно':1}
+            return table[self.Value]
+
+        if self.Type == 'adress':
+            return -1
+
+
+
