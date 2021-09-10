c5fdc89a360d032bd2971c4b1c6d2a4a06cf7340
legendaOS <robolegenda@gmail.com>
2021-09-10 20:00:01 +0300

    почти готовое древо

--- main.py
+++ main.py
@@ -27,7 +27,14 @@ for l in f:
     kv[buf[1]].hopa(kv[buf[0]])
     
     
-
+f.close()
+f = open('product.txt', 'r', encoding='UTF-8')
+for l in f:
+    buf = l.split(';')
+    buf[-1] = buf[-1].strip()
+    cat = buf[4].split(',')
+    nnn = t.Product(buf[1], buf[2], bool(int(buf[3])), cat, buf[5], buf[6], None, None, buf[0])
+    nnn.Parent = kv[buf[-1]]
 
 
     
