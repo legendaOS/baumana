5a9aec88256660d60a3767b5ec19fa0cebed0e58
FedOS <76245715+legendaOS@users.noreply.github.com>
2022-05-23 15:02:32 +0300

    отчет по параболам

--- mathMetods/lab3.m
+++ mathMetods/lab3.m
@@ -1,6 +1,6 @@
 function lab3()
-    %FX = @(x) (cos(power(x,5) - x + 3 + power(2, 1/3)) + atan( (power(x,3) - 5 * sqrt(2)*x - 4) / (sqrt(6)*x + sqrt(x)) ) + 1.8);
-    FX = @(x) (x-0.777).^12;
+    FX = @(x) (cos(power(x,5) - x + 3 + power(2, 1/3)) + atan( (power(x,3) - 5 * sqrt(2)*x - 4) / (sqrt(6)*x + sqrt(x)) ) + 1.8);
+    %FX = @(x) (x-0.777).^12;
     clc();
 
     a = 0;
